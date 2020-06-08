9031bddcb80c71cd7f6852496068101b7704e0f9
Jenny Sugden <jensugden@gmail.com>
2020-06-08 11:36:23 -0700

    meta tag update

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Seattle Loves, a site for people who love everything about Seattle.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
